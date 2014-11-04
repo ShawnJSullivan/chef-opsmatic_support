9ea89df81816b774779130d4a8185c015ec3e148
David Adams <daadams@gannett.com>
2014-11-04 14:58:08 -0500

    Pushing fork for opsmatic_support.

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+opsmatic_support CHANGELOG
+==========================
+
+This file is used to list changes made in each version of the opsmatic_support cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of opsmatic_support
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: opsmatic_support
+# Recipe:: default
+#
+# Copyright 2014, Gannett
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+##Create script to update /etc/opsmatic-agent.conf dynamically
+
+##Read in array of Groups
+##Add Environment from box to array and deploy
+
+template "/root/opsmatic_config.sh" do
+  source "opsmatic_config.sh.erb"
+  mode 0700
+end
+
+execute 'run opsmatic config' do
+  command "/root/./opsmatic_config.sh"
+end
+
+cron "opsmatic_config_reboot_command" do
+  time :reboot
+  action :create
+  command "/root/./opsmatic_config.sh"
+end
+
